ae9b2d8bd7907596c7ba18f4ffecb6c32272bd88
bhaskerrai <brai285@gmail.com>
2023-12-01 12:17:42 +0500

    some changes

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,7 @@ import mongoose from 'mongoose';
 import { Book } from './models/bookModel.js';
 import booksRoute from './routes/booksRoute.js';
 import cors from 'cors';
+
 const app = express();
 
 app.use(express.json());  //middleware
@@ -41,3 +42,5 @@ mongoose
     .catch((error) => {
         console.log(error)
     })
+
+export default app
